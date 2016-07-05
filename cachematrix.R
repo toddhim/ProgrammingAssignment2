a219dc3fbcb31e8b0f8df60c1f01af9e1e6167a9
toddhim <toddhim@hotmail.com>
2016-07-05 12:49:52 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Caching the inverse of a matrix,
+##using the <<- operator to assign a value to a different environment
 
-## Write a short comment describing this function
+## Creating a special "matrix", which is a list containing a function to do:
+        ##set the values of the matrix
+        ##get the values of the matrix
+        ##set the value of the inverse of the matrix
+        ##get the value of the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+        i <- NULL 
+        set <- function(y) { 
+                x <<- y 
+                i <<- NULL 
+        } 
+        get <- function() x 
+        setinverse <- function(inv) i <<- inv 
+        getinverse <- function() i 
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) 
+} 
 
-}
+## Calculate the inverse of the special "matrix" created with the above 
+## function, reusing cached result if it is available 
+
+cacheSolve <- function(x, ...) { 
+        i <- x$getinverse() 
+        if(!is.null(i)) { 
+                message("getting cached data") 
+                return(i) 
+        } 
+        m <- x$get() 
+        i <- solve(m, ...) 
+        x$setinverse(i) 
+        i 
+} 
+
+## Testing:
+## testmatrix <- matrix(c(5, 0, 0, 5), c(2, 2))
+## m <- makeCacheMatrix(testmatrix) 
+## cacheSolve(m) 
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
